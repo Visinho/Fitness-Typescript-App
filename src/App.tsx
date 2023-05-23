5919a064ac7afe69746a40f7d8cd0670d25994ea
Visinho <Elvisvisinho@rocketmail.com>
2023-05-23 11:00:28 +0100

    Adding the types to the navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Navbar from "@/scenes/navbar";
+import { useState } from "react";
 
 function App() {
 
+  const [selectedPage, setSelectedPage] = useState("benefits");
+
   return (
     <div className='app bg-gray-20'>
-      <Navbar />
+      <Navbar 
+      selectedPage={selectedPage}
+      setSelectedPage={setSelectedPage}/>
     </div>
   )
 }
--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,19 +1,55 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/20/solid";
 import Logo from "@/assets/Logo.png";
+import Link from "./Link";
 
-type Props = {}
+type Props = {
+  selectedPage: string;
+  setSelectedPage: (value: string) => void;
+};
 
-const Navbar = (props: Props) => {
+const Navbar = ({ selectedPage, setSelectedPage}: Props) => {
   const flexBetween = "flex items-center justify-between";
 
   return (
     <nav>
       <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
-        <div></div>
+        <div className={`${flexBetween} mx-auto w-5/6`}>
+          <div className={`${flexBetween} w-full gap-16`}>
+            <img alt="logo" src={Logo} />
+            <div className={`${flexBetween} w-full`}>
+              <div className={`${flexBetween} gap-8 text-sm`}>
+                <Link
+                  page="Home"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Benefits"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Our Classes"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+                <Link
+                  page="Contact Us"
+                  selectedPage={selectedPage}
+                  setSelectedPage={setSelectedPage}
+                />
+              </div>
+              <div className={`${flexBetween} gap-8`}>
+                <p>Sign In</p>
+                <button>Become a Member</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
