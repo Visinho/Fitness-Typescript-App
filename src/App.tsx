592c0397b67a040bb9f58f70d0f6e50be6ba01ea
Visinho <Elvisvisinho@rocketmail.com>
2023-05-31 17:38:08 +0100

    Completion of the complex navbar

--- src/App.tsx
+++ src/App.tsx
@@ -7,11 +7,22 @@ function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
 
-  useEffect
+  useEffect(() => {
+    const handleScroll = () => {
+      if(window.scrollY === 0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.scrollY !== 0)setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, []);
 
   return (
     <div className='app bg-gray-20'>
       <Navbar 
+      isTopOfPage = {isTopOfPage}
       selectedPage={selectedPage}
       setSelectedPage={setSelectedPage}/>
     </div>
