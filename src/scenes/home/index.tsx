04feb8dc7411de08145dfe78a950bf708323d5ab
Visinho <Elvisvisinho@rocketmail.com>
2023-05-31 18:54:13 +0100

    Designing the landing page

---
+++ src/scenes/home/index.tsx
@@ -0,0 +1,56 @@
+import { SelectedPage } from '@/shared/types'
+import useMediaQuery from '@/hooks/useMediaQuery';
+import ActionButton from '@/shared/ActionButton';
+import HomePageText from "@/assets/HomePageText.png";
+import HomePageGraphic from "@/assets/HomePageGraphic.png";
+import SponsorRedBull from "@/assets/SponsorRedBull.png";
+import SponsorForbes from "@/assets/SponsorForbes.png";
+import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from 'react-anchor-link-smooth-scroll';
+
+type Props = {
+    setSelectedPage: (value: SelectedPage) => void
+};
+
+const Home = (setSelectedPage: Props) => {
+    const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+  return (
+    <section
+        id="home"
+        className='gap-16 bg-gray-20 py-10 md:h-full md:pb-10'
+    >
+        <div>
+           <div>
+                <div>
+                    <div>
+                        <div>
+                            <img alt='home-page-text' src={HomePageText}/>
+                        </div>
+                    </div>
+                       <p>
+                       Unlock Your Potential with Our Gym Fitness Website!
+                            Are you ready to transform your body, boost your energy levels, and achieve your fitness goals? Look no further! Welcome to our cutting-edge gym fitness website, where we provide you with everything you need to embark on your journey to a healthier, stronger, and more confident you.
+                       </p>
+                </div>
+               <div>
+                    <ActionButton setSelectedPage={setSelectedPage}>
+                        Join Now
+                    </ActionButton>
+                    <AnchorLink
+                        className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
+                        onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+                        href={`#${SelectedPage.ContactUs }`}
+                        >
+                            <p>Learn More</p>
+                        </AnchorLink>
+               </div> 
+           </div>
+           <div>
+
+           </div>
+        </div>
+    </section>
+  )
+}
+
+export default Home;
