a40a00499eeb8bbcc82089c8245494cfb9386255
Visinho <Elvisvisinho@rocketmail.com>
2023-06-06 09:00:50 +0100

    Adding my tailwind CSS classes

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -19,20 +19,20 @@ const Home = (setSelectedPage: Props) => {
         id="home"
         className='gap-16 bg-gray-20 py-10 md:h-full md:pb-10'
     >
-        <div>
-           <div>
-                <div>
-                    <div>
-                        <div>
+        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+           <div className='z-10 mt-32 md:basis-3/5'>
+                <div className='md:-mt-20'>
+                    <div className='relative'>
+                        <div className='before:absolute before:-top-20 md:before:content-evolvetext before:-left-20 before:z-[-1]'>
                             <img alt='home-page-text' src={HomePageText}/>
                         </div>
                     </div>
-                       <p>
+                       <p className='mt-8 text-sm'>
                        Unlock Your Potential with Our Gym Fitness Website!
                             Are you ready to transform your body, boost your energy levels, and achieve your fitness goals? Look no further! Welcome to our cutting-edge gym fitness website, where we provide you with everything you need to embark on your journey to a healthier, stronger, and more confident you.
                        </p>
                 </div>
-               <div>
+               <div className='mt-8 flex items-center gap-8'>
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -40,15 +40,26 @@ const Home = (setSelectedPage: Props) => {
                         className='text-sm font-bold text-primary-500 underline hover:text-secondary-500'
                         onClick={() => setSelectedPage(SelectedPage.ContactUs)}
                         href={`#${SelectedPage.ContactUs }`}
-                        >
+                        > 
                             <p>Learn More</p>
                         </AnchorLink>
                </div> 
            </div>
            <div>
-
+                <img alt='home-page-graphic' src={HomePageGraphic} />
            </div>
         </div>
+        {isAboveMediumScreens && (
+            <div>
+                <div>
+                   <div>
+                        <img alt='redbull-sponsor' src={SponsorRedBull} />
+                        <img alt='forbes-sponsor' src={SponsorForbes} />
+                        <img alt='fortune-sponsor' src={SponsorFortune} />
+                    </div> 
+                </div>
+            </div> 
+        )}
     </section>
   )
 }
