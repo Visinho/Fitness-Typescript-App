eacd518c3c1f520d8704d727aa43039531057ef4
Visinho <Elvisvisinho@rocketmail.com>
2023-06-06 18:57:11 +0100

    Added the framer motion animations

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -7,21 +7,34 @@ import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import { motion } from 'framer-motion';
 
 type Props = {
     setSelectedPage: (value: SelectedPage) => void
 };
 
-const Home = (setSelectedPage: Props) => {
+const Home = ({setSelectedPage}: Props) => {
     const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
   return (
     <section
         id="home"
         className='gap-16 bg-gray-20 py-10 md:h-full md:pb-0'
     >
-        <div className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'>
+        <motion.div 
+        className='md:flex mx-auto w-5/6 items-center justify-center md:h-5/6'
+        onViewportEnter={() => setSelectedPage(SelectedPage.Home)}
+        >
            <div className='z-10 mt-32 md:basis-3/5'>
-                <div className='md:-mt-20'>
+                <motion.div className='md:-mt-20'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ duration: 2.0 }}
+                    variants={{ 
+                        hidden: { opacity: 0, x: -50},
+                        visible: { opacity: 1, x: 0}
+                    }}
+                    >
                     <div className='relative'>
                         <div className='before:absolute before:-top-20 md:before:content-evolvetext before:-left-20 before:z-[-1]'>
                             <img alt='home-page-text' src={HomePageText}/>
@@ -31,8 +44,18 @@ const Home = (setSelectedPage: Props) => {
                        Unlock Your Potential with Our Gym Fitness Website!
                             Are you ready to transform your body, boost your energy levels, and achieve your fitness goals? Look no further! Welcome to our cutting-edge gym fitness website, where we provide you with everything you need to embark on your journey to a healthier, stronger, and more confident you.
                        </p>
-                </div>
-               <div className='mt-8 flex items-center gap-8'>
+                </motion.div>
+               <motion.div 
+                    className='mt-8 flex items-center gap-8'
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    transition={{ delay: 1.0, duration: 2.0 }}
+                    variants={{ 
+                        hidden: { opacity: 0, x: -50},
+                        visible: { opacity: 1, x: 0}
+                    }}
+                    >
                     <ActionButton setSelectedPage={setSelectedPage}>
                         Join Now
                     </ActionButton>
@@ -43,14 +66,14 @@ const Home = (setSelectedPage: Props) => {
                         > 
                             <p>Learn More</p>
                         </AnchorLink>
-               </div> 
+               </motion.div> 
            </div>
            <div className='flex basis-3/5 justify-center md:z-10 md:ml-40 md:mt-16 md:justify-items-end'>
                 <img alt='home-page-graphic' src={HomePageGraphic} />
            </div>
-        </div>
+        </motion.div>
         {isAboveMediumScreens && (
-            <div className='h-[-150px] w-full bg-primary-100 py-10 mt-20'>
+            <div className='h-[-150px] w-full bg-primary-100 py-10 mt-10'>
                 <div className='mx-auto w-5/6'>
                    <div className='flex w-3/5 items-center justify-between gap-8'>
                         <img alt='redbull-sponsor' src={SponsorRedBull} />
